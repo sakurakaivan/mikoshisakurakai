164d0c6c86c11550036d79589e9ed21b2c9100d8
Napat <napat.jivakanun123@gmail.com>
2022-03-17 19:43:52 +0700

    first commit

---
+++ contact-frm-handler-jp.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'info@vancouversakurakai.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n". 
+                    "User Email: $visitor_email.\n".
+                        "User Message : $message.\n";
+    
+
+    $to = "info@vancouversakurakai.com"
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location contactus_jp.html");
+
+?>
